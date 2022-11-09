1455d0d0226a3c5d5120fa3a379dc7090d9cdf17
unknown <trinhanhtu01@gmail.com>
2022-11-09 15:20:25 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require('./config/db');
 const route = require('./routes');
 
